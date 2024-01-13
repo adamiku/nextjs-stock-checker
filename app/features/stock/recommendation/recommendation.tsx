28df1d5c00c34bdeaff7bd7c1723cd5ea580501b
Adam <adam_mikulas@mckinsey.com>
2024-01-14 00:58:44 +0100

    fix: remove same symbol

--- app/features/stock/recommendation/recommendation.tsx
+++ app/features/stock/recommendation/recommendation.tsx
@@ -10,15 +10,17 @@ async function Recommendation({ symbol }: Props) {
 
   return (
     <article className="flex flex-wrap gap-3">
-      {response?.map((peer) => (
-        <Link
-          key={peer}
-          href={`?query=${peer}`}
-          className="text-black bg-white p-2 rounded-lg min-w-20 text-center hover:bg-orange"
-        >
-          {peer}
-        </Link>
-      ))}
+      {response
+        ?.filter((peer) => peer.toLowerCase() !== symbol.toLowerCase())
+        .map((peer) => (
+          <Link
+            key={peer}
+            href={`?query=${peer}`}
+            className="text-black bg-white p-2 rounded-lg min-w-20 text-center hover:bg-orange"
+          >
+            {peer}
+          </Link>
+        ))}
       {!response ||
         (response.length === 0 && (
           <div>There are no peer companies for {symbol}</div>
