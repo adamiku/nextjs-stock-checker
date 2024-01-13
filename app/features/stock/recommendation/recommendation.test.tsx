943b9e0717803f126385103b808c7b0611b8ad1e
Adam <adam_mikulas@mckinsey.com>
2024-01-13 23:20:23 +0100

    test: adjust recommendation spec file

--- app/features/stock/recommendation/recommendation.test.tsx
+++ app/features/stock/recommendation/recommendation.test.tsx
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react';
 import Recommendation from './recommendation';
 
-test('renders Recommendation component', () => {
-  const { container } = render(<Recommendation />);
+test('renders Recommendation component', async () => {
+  const jsx = await Recommendation({ symbol: 'AAPL' });
+  const { container } = render(jsx);
   expect(container.firstChild).toBeInTheDocument();
 });
