4e0147313654f467dd66ab40162ecffbafe4d19b
Adam <adam_mikulas@mckinsey.com>
2024-01-13 23:21:48 +0100

    test: mock fetch in recommendation fetch

--- app/features/stock/recommendation/recommendation.test.tsx
+++ app/features/stock/recommendation/recommendation.test.tsx
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react';
 import Recommendation from './recommendation';
 
+jest.mock('@/app/lib/api', () => ({
+  __esModule: true,
+  api: {
+    get: jest.fn().mockReturnValue([])
+  }
+}));
+
 test('renders Recommendation component', async () => {
   const jsx = await Recommendation({ symbol: 'AAPL' });
   const { container } = render(jsx);
